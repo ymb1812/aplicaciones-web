a5d7564bcf8b2fcb52c4b70668dde1c55a883f0a
ymb1812 <yerely1812@gmail.com>
2025-01-18 19:29:23 -0600

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,18 @@
+package org.example.clase1;
+
+public class Variables {
+    public static void main(String[] args) {
+        boolean booleanVar = true;
+        System.out.println(booleanVar);
+
+        int initValue;
+        initValue = 100;
+        System.out.println(initValue);
+
+        long hugeValue = 10000000L;
+        System.out.println(hugeValue);
+
+        float floatVar = 3.14f;
+        System.out.println(floatVar);
+    }
+}
