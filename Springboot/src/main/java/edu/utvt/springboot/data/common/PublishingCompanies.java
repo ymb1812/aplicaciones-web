a11cfaf55a462046e0b47777964216063257dc9a
ymb1812 <yerely1812@gmail.com>
2025-03-16 12:27:53 -0600

    Add files via upload

---
+++ Springboot/src/main/java/edu/utvt/springboot/data/common/PublishingCompanies.java
@@ -0,0 +1,10 @@
+package edu.utvt.springboot.data.common;
+
+public enum PublishingCompanies {
+
+    SIMON_AND_SCHUSTER,
+    MACMILLAN,
+    PEGUIN_RANDOM_HOUSE_LLC,
+    PEARSON
+
+}
---
+++ Springboot/src/main/java/edu/utvt/springboot/data/common/RatingOptions.java
@@ -0,0 +1,9 @@
+package edu.utvt.springboot.data.common;
+
+public enum RatingOptions {
+
+    NOT_FOR_ME,
+    I_LIKE_THIS,
+    I_LOVE_THIS
+
+}
